d95d04ddd7bac92c35cc5e7fc9534f90bd065d13
Ben Sassi <Mouadh.BenSassi@Kantarmedia.com>
2016-12-06 13:55:04 +0100

    Add an Express website

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
 var slackTerminal = require('slack-terminalize');
 
-slackTerminal.init(process.env.BOT_API_KEY || 'xoxb-109593544883-SUhQ5vS5vDxbXaVSRP4clzGj', {
+slackTerminal.init(process.env.BOT_API_KEY || 'xoxb-109593544883-F72wAvMJiNWJtKw76j9DJIc1', {
     // slack rtm client options here
     // more info at: https://github.com/slackhq/node-slack-client/blob/master/lib/clients/rtm/client.js
 }, {
@@ -10,3 +26,5 @@ slackTerminal.init(process.env.BOT_API_KEY || 'xoxb-109593544883-SUhQ5vS5vDxbXaV
 	CONFIG_DIR: __dirname + '/config',
 	COMMAND_DIR: __dirname + '/commands'
 });
+
+
